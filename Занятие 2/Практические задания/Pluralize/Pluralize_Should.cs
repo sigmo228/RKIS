84cc226e1b898d44dec2d692da66adb8e9bfd51d
sigmo228 <kostyastarenkov@gmail.com>
2024-11-20 16:47:41 +0300

    Update Pluralize_Should.cs

--- Занятие 2/Практические задания/Pluralize/Pluralize_Should.cs
+++ Занятие 2/Практические задания/Pluralize/Pluralize_Should.cs
@@ -1,27 +1,32 @@
-﻿using NUnit.Framework;
 
-namespace Pluralize;
+﻿using System;
+using System.IO;
+using NUnit.Framework;
 
-[TestFixture]
-public class Pluralize_Should
+namespace Pluralize
 {
-    [Test]
-    public void BeCorrect()
-    {
-        // Это пример ввода сложных данных из файла.
-        // Циклы, строки, массивы будут рассмотрены на лекциях чуть позже, но это не должно быть препятствием вашему любопытству! :)
-        var lines = File.ReadAllLines("rubles.txt");
 
-        foreach (var line in lines)
+    [TestFixture]
+    public class Pluralize_Should
+    {
+        [Test]
+        public void BeCorrect()
         {
-            var words = line.Split(' ');
-            var count = int.Parse(words[0]);
-            var rightAnswer = words[1];
-            var pluralizedRubles = PluralizeTask.PluralizeRubles(count);
-            if (pluralizedRubles != rightAnswer)
+            // Это пример ввода сложных данных из файла.
+            // Циклы, строки, массивы будут рассмотрены на лекциях чуть позже, но это не должно быть препятствием вашему любопытству! :)
+            var lines = File.ReadAllLines("rubles.txt");
+
+            foreach (var line in lines)
             {
-                //сравниваем полученный результат с ожидаемым
-                Assert.AreEqual(rightAnswer, pluralizedRubles);
+                var words = line.Split(' ');
+                var count = int.Parse(words[0]);
+                var rightAnswer = words[1];
+                var pluralizedRubles = PluralizeTask.PluralizeRubles(count);
+                if (pluralizedRubles != rightAnswer)
+                {
+                    //сравниваем полученный результат с ожидаемым
+                    Assert.AreEqual(rightAnswer, pluralizedRubles);
+                }
             }
         }
     }
